55fd0806b026bf37114f0e88003bade7528c2c1b
Guillermo Rojas Lema <guillermorojaslema@gmail.com>
2013-04-16 14:10:01 -0200

    Realizado procedimientos hasta la primera resta del vector dispoibilidad de recursos

--- CppApplication_1/main.c
+++ CppApplication_1/main.c
@@ -15,36 +15,70 @@ typedef struct {
     int matriz [10][4];
 }matrix;
 
-void fill_matrix (matrix* generic_matrix, int resources_value, int process_value){
+void fill_matrix (matrix* generic_matrix, int resources_quantity, int process_quantity){
     int i, j, values;
-    for (i=0; i<process_value; i++)
-        for (j=0; j<resources_value; j++){
+    for (i=0; i<process_quantity; i++)
+        for (j=0; j<resources_quantity; j++){
         
-            printf("Escriba un valor para el recurso R",j"Y el proceso P", i); /*Crear las excepciones para valores monstruosos*/
+            printf("Escriba un valor para el recurso R",j);printf("Y el proceso P", i); /*Crear las excepciones para valores monstruosos*/
             scanf("%d",&values);
             generic_matrix->matriz[i][j];
         }        
     
 }
 
-void need_matrix(matrix need, matrix maximum, matrix assigned, int resources_value, int process_value){
+void need_matrix(matrix* need, matrix maximum, matrix assigned, int resources_quantity, int process_quantity){
     int i, j;
-    for (i=0; i<process_value; i++);
-        for (j=0; j<resources_value; j++);
+    for (i=0; i<process_quantity; i++);
+        for (j=0; j<resources_quantity; j++);
         need->matriz[i][j]=maximum.matriz[i][j]-assigned.matriz[i][j];
 }
 
+void fill_disponibility(int* disponibility, int resources_quantity){
+    int i=0, value_disponibility; 
+    do{
+        printf("Escriba un valor para el recurso R", i);
+        scanf("%d", &value_disponibility);
+        disponibility[i]= value_disponibility;
+        i++;
+    } while (i<resources_quantity);
+    
+}
+void select_from_maximum (matrix assigned, int* disponibility, int resources_quantity, int process_quantity, int* auxiliar){
+    int i, j, aux=0, auxiliar[resources_quantity];
+    for(j=0; j<process_quantity; j++){
+        for(i=0; i<resources_quantity; i++)
+              aux = aux+assigned.matriz[i][j];
+        int auxiliar[j]= aux;
+    }
+}
+
+void first_step(int* disponibility, int* auxiliar, int resources_quantity ){
+    int i; 
+    for(i=0; i<resources_quantity; i++){
+        disponibility[i]= disponibility[i]- auxiliar[i];
+    }
+}
+
+void matching_need (int* disponibility, matrix need){
+    
+}
+
 int main(int argc, char** argv) {
-    int resources_value, process_value;
+    int resources_quantity, process_quantity,i=0;
     matrix assigned, maximum, need;
-    printf("Escriba Cantidad de Recursos. Máximo 4"); /*Crear las excepciones*/
-    scanf("%d", &resources_value);
-    printf("Escriba Cantidad de Procesos. Máximo 10"); /*Crear las excepciones*/
-    scanf("%d", &process_value);
+    printf("Escriba Cantidad de Recursos. Máximo 4"); /*Crear las excepciones con do-while*/
+    scanf("%d", &resources_quantity);
+    int disponibility[resources_quantity];
+    int auxiliar[resources_quantity];
+    printf("Escriba Cantidad de Procesos. Máximo 10"); /*Crear las excepciones con do-while*/
+    scanf("%d", &process_quantity);
+    
     
     
     
     
     return (EXIT_SUCCESS);
 }
+
 
